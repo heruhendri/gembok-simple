41952566479992a0ecd2cf62325d25190a7bb200
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2026-02-27 10:20:57 +0700

    Add files via upload

--- sales/login.php
+++ sales/login.php
@@ -36,6 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
+$appName = getSetting('app_name', 'GEMBOK');
 $pageTitle = 'Login Sales';
 ?>
 <!DOCTYPE html>
@@ -43,7 +44,7 @@ $pageTitle = 'Login Sales';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle; ?> - <?php echo APP_NAME; ?></title>
+    <title><?php echo $pageTitle; ?> - <?php echo htmlspecialchars($appName); ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <style>
